c4e71c375ab4c32c5827cd3d7e2ed9ddb9d213b4
Tommy Lowry <lowrytommy@gmail.com>
2025-11-24 21:18:48 -0500

    use the name of the app for the app in procfile

--- patriot_center_frontend/src/services/options.js
+++ patriot_center_frontend/src/services/options.js
@@ -1,5 +1,6 @@
+const BASE = process.env.REACT_APP_API_BASE || '';
 export async function fetchOptions() {
-    const res = await fetch('/meta/options');
-    if (!res.ok) throw new Error('HTTP ' + res.status);
-    return res.json();
-  }
+  const res = await fetch(`${BASE}/meta/options`);
+  if (!res.ok) throw new Error('HTTP ' + res.status);
+  return res.json();
+}

00ebdcb8c1cda59a26c8d0f03f2cf8c8143a8713
Tommy Lowry <lowrytommy@gmail.com>
2026-01-11 22:24:20 -0500

    unit testing done for sleeper helpers

---
+++ patriot_center_backend/tests/utils/test_sleeper_helpers.py
@@ -0,0 +1,436 @@
+"""Unit tests for sleeper_helpers module."""
+
+from unittest.mock import call, patch
+
+import pytest
+
+from patriot_center_backend.utils.sleeper_helpers import (
+    fetch_sleeper_data,
+    get_current_season_and_week,
+    get_roster_id,
+    get_roster_ids,
+)
+
+
+@pytest.fixture(autouse=True)
+def globals_setup():
+    """
+    Setup common mocks for all tests.
+
+    These mocks include:
+
+    - mock_league_ids: a mock of LEAGUE_IDS
+    - mock_name_to_manager_username: a mock of NAME_TO_MANAGER_USERNAME
+    - mock_username_to_real_name: a mock of USERNAME_TO_REAL_NAME
+
+    The mocks are set up to return a pre-defined set of values when accessed.
+    """
+    # Mock values
+    mock_username_to_real_name = {
+        "username1": "Manager 1",
+        "username2": "Manager 2",
+        "username3": "Manager 3"
+    }
+    mock_league_ids = {
+        2023: "leagueid2023",
+        2024: "leagueid2024"
+    }
+
+    # Patch the mocks
+    with patch('patriot_center_backend.utils.sleeper_helpers.LEAGUE_IDS', mock_league_ids), \
+         patch('patriot_center_backend.utils.sleeper_helpers.USERNAME_TO_REAL_NAME', mock_username_to_real_name):
+        yield
+
+
+class TestFetchSleeperData:
+    """Unit tests for fetch_sleeper_data function."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """
+        Setup common mocks for all tests.
+
+        This fixture sets up the following mocks:
+
+        - mock_get: a mock of requests.get
+
+        The mocks are set up to return a pre-defined set of values when accessed.
+        """
+        with patch('patriot_center_backend.utils.sleeper_helpers.requests.get') as mock_get:
+            self.mock_get = mock_get
+            yield
+    
+    def test_fetch_sleeper_data(self):
+        """
+        Unit test for fetch_sleeper_data function.
+
+        Tests that the fetch_sleeper_data function calls the correct URL and returns the expected JSON response.
+        """
+        self.mock_get.return_value.json.return_value = {}
+        self.mock_get.return_value.status_code = 200
+        
+        fetch_sleeper_data('test_endpoint')
+
+        self.mock_get.assert_called_once_with('https://api.sleeper.app/v1/test_endpoint')
+    
+    def test_fetch_sleeper_data_connection_error(self):
+        """
+        Unit test for fetch_sleeper_data function.
+
+        Tests that the fetch_sleeper_data function raises a ConnectionAbortedError when the request to the Sleeper API fails.
+        """
+        self.mock_get.return_value.json.return_value = {}
+        self.mock_get.return_value.status_code = 500
+        
+        with pytest.raises(ConnectionAbortedError,
+                           match="Failed to fetch data from Sleeper API with call to https://api.sleeper.app/v1/test_endpoint"):
+            fetch_sleeper_data('test_endpoint')
+
+
+class TestGetRosterId:
+    """Unit tests for get_roster_id function."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """
+        Setup common mocks for all tests.
+
+        These mocks include:
+
+        - mock_fetch_sleeper_data: a mock of fetch_sleeper_data function
+
+        The mocks are set up to return a pre-defined set of values when accessed.
+        """
+        with patch('patriot_center_backend.utils.sleeper_helpers.fetch_sleeper_data') as mock_fetch_sleeper_data:
+            self.mock_fetch_sleeper_data = mock_fetch_sleeper_data
+            yield
+
+    def test_get_roster_id(self):
+        """
+        Unit test for get_roster_id function.
+
+        Tests that the get_roster_id function returns the correct roster ID for a given user ID and year.
+
+        The test case creates a mock sleeper_rosters_response with two users, and calls the get_roster_id function with
+        the user ID 'user1' and year 2023. The test case then checks that the returned roster ID is 1.
+        """
+        sleeper_rosters_response = [
+            {'owner_id': 'user1', 'roster_id': 1},
+            {'owner_id': 'user2', 'roster_id': 2},
+        ]
+        user_id = 'user1'
+        year = 2023
+
+        result = get_roster_id(user_id, year, sleeper_rosters_response)
+
+        assert result == 1
+
+    def test_sleeper_api_not_called(self):
+        """
+        Unit test for get_roster_id function.
+
+        Tests that the get_roster_id function does not call the Sleeper API if the sleeper_rosters_response is provided.
+
+        The test case creates a mock sleeper_rosters_response with two users, and calls the get_roster_id function with
+        the user ID 'user1', year 2023, and the mock sleeper_rosters_response. The test case then checks that the
+        fetch_sleeper_data function is not called.
+        """
+        sleeper_rosters_response = [
+            {'owner_id': 'user1', 'roster_id': 1},
+            {'owner_id': 'user2', 'roster_id': 2},
+        ]
+        user_id = 'user1'
+        year = 2023
+
+        get_roster_id(user_id, year, sleeper_rosters_response)
+
+        self.mock_fetch_sleeper_data.assert_not_called()
+    
+    def test_get_roster_id_not_found(self):
+        """
+        Unit test for get_roster_id function.
+
+        Tests that the get_roster_id function returns None when the user ID is not found in the sleeper_rosters_response.
+
+        The test case creates a mock sleeper_rosters_response with two users, and calls the get_roster_id function
+        with the user ID 'user3', year 2023, and the mock sleeper_rosters_response. The test case then checks that the
+        returned roster ID is None.
+        """
+        sleeper_rosters_response = [
+            {'owner_id': 'user1', 'roster_id': 1},
+            {'owner_id': 'user2', 'roster_id': 2},
+        ]
+        user_id = 'user3'
+        year = 2023
+
+        result = get_roster_id(user_id, year, sleeper_rosters_response)
+
+        assert result is None
+
+    def test_no_sleeper_rosters_input(self):
+        """
+        Test get_roster_id function with no sleeper_rosters_input.
+
+        This test case checks that the get_roster_id function calls fetch_sleeper_data with the correct
+        arguments when no sleeper_rosters_input is provided, and that the function returns the
+        correct roster ID for the given user ID and year.
+        """
+        self.mock_fetch_sleeper_data.return_value = [
+            {'owner_id': 'user1', 'roster_id': 1},
+            {'owner_id': 'user2', 'roster_id': 2},
+        ]
+        
+        user_id = 'user1'
+        year = 2023
+
+        result = get_roster_id(user_id, year)
+
+        self.mock_fetch_sleeper_data.assert_called_once_with(f"league/leagueid2023/rosters")
+        assert result is 1
+
+
+class TestGetRosterIds:
+    """Unit tests for get_roster_ids function."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """
+        Setup common mocks for all tests.
+
+        These mocks include:
+
+        - mock_fetch_sleeper_data: a mock of fetch_sleeper_data function
+        - mock_get_roster_id: a mock of get_roster_id function
+
+        The mocks are set up to return a pre-defined set of values when accessed.
+        """
+        with patch('patriot_center_backend.utils.sleeper_helpers.fetch_sleeper_data') as mock_fetch_sleeper_data, \
+             patch('patriot_center_backend.utils.sleeper_helpers.get_roster_id') as mock_get_roster_id:
+            self.mock_fetch_sleeper_data = mock_fetch_sleeper_data
+            self.mock_get_roster_id = mock_get_roster_id
+            yield
+    
+    def test_get_roster_ids(self):
+        """
+        Test get_roster_ids function.
+
+        This test case checks that the get_roster_ids function returns the correct roster IDs
+        for the given users.
+        """
+        self.mock_fetch_sleeper_data.side_effect = [
+            [
+                {'user_id': 'user1', 'display_name': 'username1'},
+                {'user_id': 'user2', 'display_name': 'username2'},
+            ],
+            [
+                {'owner_id': 'user1', 'roster_id': 1},
+                {'owner_id': 'user2', 'roster_id': 2},
+            ]
+        ]
+        self.mock_get_roster_id.side_effect = [1, 2]
+
+        year = 2023
+        result = get_roster_ids(year)
+
+        assert result == {1: "Manager 1", 2: "Manager 2"}
+    
+    def test_get_roster_ids_not_found(self):
+        """
+        Test get_roster_ids function with one user not found in rosters data.
+
+        This test case checks that if one user is not found in the rosters data, an
+        Exception is not raised and the function returns the correct roster IDs for
+        the other users.
+        """
+        self.mock_fetch_sleeper_data.side_effect = [
+            [
+                {'user_id': 'user1', 'display_name': 'username1'},
+                {'user_id': 'user2', 'display_name': 'username2'},
+            ],
+            [
+                {'owner_id': 'user1', 'roster_id': 1},
+                {'owner_id': 'user2', 'roster_id': None},
+            ]
+        ]
+        self.mock_get_roster_id.side_effect = [1, None]
+
+        year = 2024
+        result = get_roster_ids(year)
+
+        assert result == {1: "Manager 1", 2: "Manager 2"}
+
+    def test_calls_to_sleeper_correct(self):
+        """
+        Test get_roster_ids function with calls to Sleeper API correct.
+
+        This test case checks that the get_roster_ids function makes the correct calls to the
+        Sleeper API to fetch the users data and the rosters data.
+
+        The test case also checks that the get_roster_ids function returns the correct roster IDs
+        for the given users.
+        """
+        self.mock_fetch_sleeper_data.side_effect = [
+            [
+                {'user_id': 'user1', 'display_name': 'username1'},
+                {'user_id': 'user2', 'display_name': 'username2'},
+            ],
+            [
+                {'owner_id': 'user1', 'roster_id': 1},
+                {'owner_id': 'user2', 'roster_id': 2},
+            ]
+        ]
+        self.mock_get_roster_id.side_effect = [1, 2]
+
+        year = 2023
+        get_roster_ids(year)
+
+        self.mock_fetch_sleeper_data.assert_any_call(f"league/leagueid2023/users")
+        self.mock_fetch_sleeper_data.assert_any_call(f"league/leagueid2023/rosters")
+    
+    def test_calls_to_get_roster_id_correct(self):
+        """
+        Test get_roster_ids function with calls to get_roster_id correct.
+
+        Asserts that get_roster_id is called with the correct arguments.
+        """
+        sleeper_output_2 = [
+            {'owner_id': 'user1', 'roster_id': 1},
+            {'owner_id': 'user2', 'roster_id': 2},
+        ]
+        self.mock_fetch_sleeper_data.side_effect = [
+            [
+                {'user_id': 'user1', 'display_name': 'username1'},
+                {'user_id': 'user2', 'display_name': 'username2'},
+            ],sleeper_output_2
+        ]
+        self.mock_get_roster_id.side_effect = [1, 2]
+
+        year = 2023
+        get_roster_ids(year)
+
+        calls = self.mock_get_roster_id.call_args_list
+        assert call('user1', 2023, sleeper_rosters_response=sleeper_output_2) in calls
+        assert call('user2', 2023, sleeper_rosters_response=sleeper_output_2) in calls
+
+    def test_error_gets_raised_with_missing_rosters(self):
+        """
+        Test get_roster_ids function with missing rosters.
+
+        This test case checks that if one or more users are not found in the rosters data, an
+        Exception is raised with the correct error message.
+
+        The test case creates a mock that returns users data with three users, and rosters data with
+        two users. The get_roster_ids function is then called with the year 2023, and the test case
+        checks that an Exception is raised with the correct error message.
+        """
+        self.mock_fetch_sleeper_data.side_effect = [
+            [
+                {'user_id': 'user1', 'display_name': 'username1'},
+                {'user_id': 'user2', 'display_name': 'username2'},
+                {'user_id': 'user3', 'display_name': 'username3'},
+            ],
+            [
+                {'owner_id': 'user1', 'roster_id': 1},
+                {'owner_id': 'user2', 'roster_id': 2},
+                {'owner_id': 'user3', 'roster_id': None},
+            ]
+        ]
+        self.mock_get_roster_id.side_effect = [1, 2, None]
+
+        year = 2023
+
+        with pytest.raises(Exception, match=f"Missing rosters for the following users"):
+            get_roster_ids(year)
+
+
+class TestGetCurrentSeasonAndWeek:
+    """Unit tests for get_current_season_and_week function."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """
+        Setup common mocks for all tests.
+
+        This fixture sets up the following mocks:
+
+        - mock_datetime: a mock of the datetime module
+        - mock_fetch_sleeper_data: a mock of the fetch_sleeper_data function
+
+        The mocks are yielded to each test, allowing them to be used as needed.
+        """
+        with patch('patriot_center_backend.utils.sleeper_helpers.datetime') as mock_datetime, \
+             patch('patriot_center_backend.utils.sleeper_helpers.fetch_sleeper_data') as mock_fetch_sleeper_data:
+            self.mock_datetime = mock_datetime
+            self.mock_fetch_sleeper_data = mock_fetch_sleeper_data
+            yield
+    
+    def test_valid_middle_of_the_season(self):
+        """
+        Test that get_current_season_and_week function returns the correct season and week number
+        when given a valid middle of the season date.
+        """
+        self.mock_datetime.now.return_value.year = 2023
+        self.mock_fetch_sleeper_data.return_value = {
+            'season': 2023,
+            'settings': {'last_scored_leg': 2}
+        }
+
+        result = get_current_season_and_week()
+
+        assert result == (2023, 2)
+    
+    def test_feth_sleeper_called_correct(self):
+        """
+        Test that fetch_sleeper_data is called with the correct arguments.
+        """
+        self.mock_datetime.now.return_value.year = 2023
+        self.mock_fetch_sleeper_data.return_value = {
+            'season': 2023,
+            'settings': {'last_scored_leg': 2}
+        }
+
+        get_current_season_and_week()
+
+        self.mock_fetch_sleeper_data.assert_called_once_with(f"league/leagueid2023")
+
+    def test_valid_offseason(self):
+        """
+        Test get_current_season_and_week function with offseason data.
+        """
+        self.mock_datetime.now.return_value.year = 2023
+        self.mock_fetch_sleeper_data.return_value = {
+            'season': 2023,
+            'settings': {'last_scored_leg': 17}
+        }
+
+        result = get_current_season_and_week()
+
+        assert result == (2023, 17)
+    
+    def test_valid_preseason(self):
+        """
+        Test get_current_season_and_week function with preseason data.
+        """
+        self.mock_datetime.now.return_value.year = 2023
+        self.mock_fetch_sleeper_data.return_value = {
+            'season': 2023,
+        }
+
+        result = get_current_season_and_week()
+
+        assert result == (2023, 0)
+    
+    def test_old_league(self):
+        """
+        Test that get_current_season_and_week function returns the correct
+        season and week number when given an old league ID.
+        """
+        self.mock_datetime.now.return_value.year = 2026
+        self.mock_fetch_sleeper_data.return_value = {
+            'season': 2023,
+            'settings': {'last_scored_leg': 17}
+        }
+
+        result = get_current_season_and_week()
+
+        assert result == (2023, 17)
--- patriot_center_backend/utils/sleeper_helpers.py
+++ patriot_center_backend/utils/sleeper_helpers.py
@@ -3,29 +3,31 @@ This module provides utility functions for interacting with the Sleeper API.
 """
 
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 
 import requests
 
-from patriot_center_backend.constants import LEAGUE_IDS, USERNAME_TO_REAL_NAME
+from patriot_center_backend.constants import (
+    LEAGUE_IDS,
+    USERNAME_TO_REAL_NAME,
+)
 
 SLEEPER_API_URL = "https://api.sleeper.app/v1"
 
 
 def fetch_sleeper_data(endpoint: str) -> Dict[str, Any]:
     """
-    Fetch data from the Sleeper API.
+    Fetches data from the Sleeper API given an endpoint.
 
     Args:
-        endpoint (str): The API endpoint to fetch data from.
+        endpoint: The endpoint to call on the Sleeper API.
 
     Returns:
-        dict: Parsed JSON response from the API.
+        The parsed JSON response from the Sleeper API.
 
     Raises:
-        ConnectionAbortedError: If the API call fails.
+        ConnectionAbortedError: If the request to the Sleeper API fails.
     """
-    # Construct full URL from configured base and endpoint
     url = f"{SLEEPER_API_URL}/{endpoint}"
 
     response = requests.get(url)
@@ -36,68 +38,105 @@ def fetch_sleeper_data(endpoint: str) -> Dict[str, Any]:
     # Return parsed JSON 
     return response.json()
 
-def get_roster_id(user_id: str, year: int, sleeper_rosters_response: Optional[List[Dict[str, Any]]]) -> (str | None):
+def get_roster_id(user_id: str, year: int, sleeper_rosters_response: List[Dict[str, Any]] = None) -> int | None:
     """
-    Get the roster ID for a given user ID.
+    Retrieves a roster ID for a given user ID and year.
 
     Args:
-        user_id (str): The Sleeper user ID.
-        year (int): The NFL season year.
+        user_id (str): The user ID to retrieve the roster ID for.
+        year (int): The year to retrieve the roster ID for.
+        sleeper_rosters_response (List[Dict[str, Any]], optional): The response from the Sleeper API
+            containing the rosters data. If not provided, the function will fetch the data itself.
 
     Returns:
-        str | None: The roster ID for the user, or None if not found.
+        int | None: The roster ID for the given user ID, or None if the user ID is not found.
     """
+    if sleeper_rosters_response is None:
+        sleeper_rosters_response = fetch_sleeper_data(f"league/{LEAGUE_IDS[year]}/rosters")
+
+    # Iterate over the rosters data and find the roster ID for the given user ID
     for user in sleeper_rosters_response:
         if user['owner_id'] == user_id:
             return user['roster_id']
-    
-    if year == 2024:
-        return "Davey"
     
     return None
 
 def get_roster_ids(year: int) -> Dict[int, str]:
     """
-    Get a dictionary mapping roster IDs to user names.
+    Retrieves a mapping of roster IDs to real names for a given year.
 
     Args:
-        year (int): The NFL season year.
+        year (int): The year for which to retrieve the roster IDs.
 
     Returns:
-        dict: A dictionary mapping roster IDs to user names.
+        dict: Mapping of roster IDs to real names.
+
+    Raises:
+        Exception: If not all roster IDs are assigned to a user.
     """
     user_ids = {}
+
+    # Fetch the users data from the Sleeper API
     sleeper_users_response = fetch_sleeper_data(f"league/{LEAGUE_IDS[year]}/users")
+
+    # Iterate over the users data and store the user IDs with their real names
     for user in sleeper_users_response:
         user_ids[user['user_id']] = USERNAME_TO_REAL_NAME[user['display_name']]
-    
-    roster_ids = {}
-    sleeper_rosters_response = fetch_sleeper_data(f"league/{LEAGUE_IDS[year]}/rosters")
-    for user in sleeper_rosters_response:
-        
-        roster_ids[user['roster_id']] = get_roster_id(user['owner_id'], year,
-                                                      sleeper_rosters_response=sleeper_rosters_response)
 
+    roster_ids = {}
+
+    # Fetch the rosters data from the Sleeper API
+    sleeper_rosters_response = fetch_sleeper_data(f"league/{LEAGUE_IDS[year]}/rosters")
+
+    # Iterate over the rosters data and store the roster IDs
+    users_missing = []
+    for user in sleeper_rosters_response:
+        user_id   = user['owner_id']
+        roster_id = get_roster_id(user_id, year, sleeper_rosters_response=sleeper_rosters_response)
+        
+        if not roster_id:
+            users_missing.append(user_ids[user_id])
+            continue
+
+        # Store the roster ID and the real name of the user
+        roster_ids[roster_id] = user_ids[user_id]
+
+
+    # In 2024 special case, if there is only one user missing, assign them the roster_id missing from the roster_ids
+    if year == 2024 and len(users_missing) == 1:
+        for i in range(1, len(user_ids) + 1):
+            if i not in roster_ids:
+                roster_ids[i] = users_missing[0]
+    
+
+    # If not all roster_ids are assigned to a user, raise an error
+    if len(roster_ids) != len(user_ids):
+        raise Exception(f"Missing rosters for the following users: {users_missing}")
+    
     return roster_ids
 
 def get_current_season_and_week() -> Tuple[int, int]:
     """
-    Get the current NFL season and week.
+    Retrieves the current season and week number based on the current date.
+
+    The current season is determined by the year. If the current year is not in the LEAGUE_IDS,
+    and the current month is less than 8, the previous year is used.
+    Otherwise, the current year is used.
+
+    The current week number is determined by the latest completed/scored fantasy week.
+    If the current week is in the preseason, the week number is 0.
 
     Returns:
-        Tuple[int, int]: A tuple containing the current season and week.
+        tuple: (current_season, current_week)
     """
     current_year = datetime.now().year
 
-    if current_year not in LEAGUE_IDS and datetime.now().month < 8:
-        if current_year - 1 in LEAGUE_IDS:
-            current_year -= 1
-        else:
-            raise Exception(f"No league ID found for the current year OR the previous year: {current_year}, {current_year-1}")
+    if current_year > max(LEAGUE_IDS.keys()):
+        current_year = max(LEAGUE_IDS.keys())
 
-    league_id = LEAGUE_IDS.get(int(current_year))  # Get the league ID for the current year
+    league_id = LEAGUE_IDS.get(int(current_year))
     
-    # OFFLINE DEBUGGING, comment out when online
+    # # OFFLINE DEBUGGING
     # return "2025", 10
 
     # Query Sleeper API for league metadata
